2595959beb14d0eda25a9673f78d89f8f06b3c5b
John Laurence Resando <83331866+Looreenzo0@users.noreply.github.com>
2024-10-26 23:05:06 +0800

    setup appwrite

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,13 @@
+import Heading from "@/components/Heading";
 import RoomCard from "@/components/RoomCard";
 import rooms from "@/data/rooms.json";
 
 export default function Home() {
   return (
     <>
+      <Heading title="Available Rooms" />
       {rooms.length > 0 ? (
-        rooms.map((room) => <RoomCard room={room} />)
+        rooms.map((room) => <RoomCard key={room.id} room={room} />)
       ) : (
         <p>No rooms available at the moment.</p>
       )}
